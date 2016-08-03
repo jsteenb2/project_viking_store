a274e7379b1f84284907966981a3ea37635edf69
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-03 10:51:06 -0700

    added model lookup for revenue

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,5 @@
 module DashboardsHelper
+  def midnight_tonight
+    (Time.now.to_date + 1).to_time
+  end
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -13,10 +13,17 @@ class Dashboard < ActiveRecord::Base
     end
   end
 
-  def calculate_revenue
+  def calculate_revenue(days_ago = nil)
+    if days
+      total_costs = Order.find_by_sql('SELECT SUM(products.price * order_contents.quantity) as total FROM orders JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id WHERE orders.checkout_date IS NOT NULL AND checkout_date is > ?', (midnight_tonight - days_ago))
+    else
+      total_costs = Order.find_by_sql('SELECT SUM(products.price * order_contents.quantity) as total FROM orders JOIN order_contents ON orders.id = order_contents.order_id JOIN products ON products.id = order_contents.product_id WHERE orders.checkout_date IS NOT NULL')
+    end
   end
 
   def store_days_max
 
   end
 end
+
+# x = Order.find_by_sql('SELECT * FROM orders JOIN order_contents ON orders.id = order_contents.order_id WHERE orders.checkout_date IS NOT NULL')
